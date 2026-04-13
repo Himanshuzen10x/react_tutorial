ffde2b86d7b39dfe3d8d04bd084f5d70e0ef456d
himanshukhare <himanshukhare450@gmail.com>
2026-04-13 23:56:49 +0530

    colour change

--- src/wrapper.jsx
+++ src/wrapper.jsx
@@ -1,5 +1,5 @@
 
-function Wrapper({children,color="green"}){
+function Wrapper({children,color="pink"}){
     return(
         <div style={{color:color, border:"5px solid green", width:"300px", textAlign:"center" }}>
             {children}
